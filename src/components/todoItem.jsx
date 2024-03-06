f056463c772b7cf034cb884f127c901a02c766ed
MOHAMMED RAFEEQ <mohammedrafeeqshariff17@gmail.com>
2024-03-06 14:55:10 +0530

    more-hooks-assignment

--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+// TodoItem.jsx
 
+import React from 'react';
 
-function TodoItem({ele, dispatch, index}) {
-
-    const {data, isHidden} = ele;
+function TodoItem({ ele, dispatch, index }) {
+  const { data, isHidden } = ele;
   return (
-    <div>
-        <h3>{isHidden ? "This is hidden" : data}</h3>
-        <button onClick={()=>{dispatch({type : "HIDE_DATA", payload: index})}}>Toggle</button>
+    <div className={`todo-item ${isHidden ? 'hidden' : ''}`}>
+      <h3>{isHidden ? 'This is hidden' : data}</h3>
+      <button onClick={() => dispatch({ type: 'HIDE_DATA', payload: index })}>
+        Toggle
+      </button>
     </div>
-  )
+  );
 }
 
 export default TodoItem;
--- src/components/userInput.jsx
+++ src/components/userInput.jsx
@@ -1,62 +1,58 @@
+// TodoApp.jsx
+
 import React, { useReducer, useRef } from 'react';
 import TodoItem from './todoItem';
 
 
-let initialState = [
-    // {
-    //     data: "rough",
-    //     isHidden : false
-    // }
-]
+let initialState = [];
 
 function TodoApp() {
+  const [todoData, dispatch] = useReducer(reducer, initialState);
 
-    const [todoData, dispatch] = useReducer(reducer, initialState )
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'ENTER_DATA':
+        return [...state, { data: action.payload }];
 
-    function reducer(state, action){
-        switch(action.type){
-            case "ENTER_DATA":
-                return[
-                        ...state, {data: action.payload}
-                ]
+      case 'HIDE_DATA':
+        return state.map((e, i) =>
+          i === action.payload ? { ...e, isHidden: !e.isHidden } : e
+        );
 
-            case "HIDE_DATA" :
-                return state.map((e, i)=>(
-                    i == action.payload ? {...e, isHidden : !e.isHidden}: e
-                ))
-        }  
+      default:
+        return state;
     }
+  }
 
-    console.log(todoData)
+  console.log(todoData);
 
-    const returnedReference = useRef('rough')
+  const returnedReference = useRef('rough');
   return (
-
-    <div>
-        <div>
-            <input type="text"
-             placeholder='enter your todo'
-             onKeyDown={(e)=>{
-                if(e.key == "Enter"){
-                    dispatch({type: "ENTER_DATA", payload: e.target.value})
-                }
-             }}
-             ref={returnedReference}
-             
-             />
-        </div>
-    
-        <div>
-            {
-                todoData.map((  ele,i)=>(
-                    <TodoItem ele={ele} key={i} index={i} dispatch={dispatch}/>
-                ))
+    <div className="todo-app">
+      <div>
+        <input
+          type="text"
+          placeholder="Enter your todo"
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              dispatch({ type: 'ENTER_DATA', payload: e.target.value });
+              returnedReference.current.value = ''; // Clear the input after adding todo
             }
-        </div>
-        <button onClick={()=>{returnedReference.current.focus()}}>Go back to top</button>
+          }}
+          ref={returnedReference}
+        />
+      </div>
+
+      <div>
+        {todoData.map((ele, i) => (
+          <TodoItem ele={ele} key={i} index={i} dispatch={dispatch} />
+        ))}
+      </div>
+      <button onClick={() => returnedReference.current.focus()}>
+        Go back to top
+      </button>
     </div>
-    
-  )
+  );
 }
 
 export default TodoApp;
